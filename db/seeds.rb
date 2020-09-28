b16d9ca7250c9a2fd288e63d2ec04878c5061734
Ubuntu <ubuntu@ip-172-31-45-94.us-east-2.compute.internal>
2020-09-28 10:57:31 +0000

    getting ready to deploy in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "shahid", password: "password")
+User.create(username: "nicky", password: "password")
+User.create(username: "raj", password: "password")
+User.create(username: "Zidaan", password: "password")
